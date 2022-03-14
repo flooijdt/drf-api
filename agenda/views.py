3e0473e4da8086467ae8c32d92126231f129af0d
flooijdt <flooijdt@posteo.de>
2022-03-14 15:04:30 -0300

    adicionada a view padrao json

--- agenda/views.py
+++ agenda/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from agenda.models import Agendamento
+from agenda.serializers import AgendamentoSerializer
+from rest_framework.decorators import api_view
 # Create your views here.
+
+@api_view(http_method_names=["GET"])
+def agendamento_detail(request, id):
+    obj = get_object_or_404(Agendamento, id=id)
+    serializer = AgendamentoSerializer(obj)
+    return JsonResponse(serializer.data)
+
+
+@api_view(http_method_names=["GET"])
+def agendamento_list(request):
+    qs = Agendamento.objects.all()
+    serializer = AgendamentoSerializer(qs, many=True)
+    return JsonResponse(serializer.data, safe=False)
