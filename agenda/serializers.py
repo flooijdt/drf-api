ed1779dd1ae6be7659d5db6e41750911d156f5ed
flooijdt <flooijdt@posteo.de>
2022-03-17 14:03:03 -0300

    alteracoes pedidas pelo gabriel  efetuadas

--- agenda/serializers.py
+++ agenda/serializers.py
@@ -18,16 +18,8 @@ class AgendamentoSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("Agendamento nao pode ser feito no passado!")
         return value
 
-    
-    def validate(self, attrs):
-        telefone_cliente = attrs.get("telefone_cliente", "")
-        email_cliente = attrs.get("email_cliente", "")
-        data_horario = attrs.get("data_horario", "")
-        dia = attrs.get("data_horario", "").date()
-
-        if email_cliente.endswith(".br") and telefone_cliente.startswith("+") and not telefone_cliente.startswith("+55"):
-            raise serializers.ValidationError("Email brasileiro deve estar associado a um numero brasileiro (+55)")
-        
+    def validate_telefone_cliente(self, value):
+        telefone_cliente = value
         if len(telefone_cliente) < 8:
             raise serializers.ValidationError("Número de telefone deve conter mais de 7 dígitos.")
         
@@ -36,13 +28,24 @@ class AgendamentoSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError("Caractere de telefone não aceito.")
         if "+" in telefone_cliente and telefone_cliente[0] is not "+":
             raise serializers.ValidationError("O sinal de '+' apenas pode ser utilizado no começo do número de telefone.")
+        return value
+
+    def validate_data_horario(self, value):
+        for i in Agendamento.objects.all().datetimes("data_horario", "minute"):
+            if value - i < timedelta(minutes=30):
+                raise serializers.ValidationError("Já existem outros agendamentos neste horário. Escolha outro horário.")
+        return value
+     
+    def validate(self, attrs):
+        telefone_cliente = attrs.get("telefone_cliente", "")
+        email_cliente = attrs.get("email_cliente", "")
+        data_horario = attrs.get("data_horario", "")
+
+        if email_cliente.endswith(".br") and telefone_cliente.startswith("+") and not telefone_cliente.startswith("+55"):
+            raise serializers.ValidationError("Email brasileiro deve estar associado a um numero brasileiro (+55)")
         
         if data_horario.date() in Agendamento.objects.filter(email_cliente=email_cliente).dates("data_horario", 'day'):# o ideal seria iterar pelos objetos com email igual e verificar se a data_horario.date() coincide
             raise serializers.ValidationError("Não se pode fazer mais de um agendamento diário.")
-        
-        for i in Agendamento.objects.all().datetimes("data_horario", "minute"):
-            if data_horario - i < timedelta(minutes=30):
-                raise serializers.ValidationError("Já existem outros agendamentos neste horário. Escolha outro horário.")
         
         return attrs
 
